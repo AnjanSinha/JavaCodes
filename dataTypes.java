b93e2f71ca17e647a34890f8bd8c4367d14c38f6
AnjanSinha <sinhaanjan2002@gmail.com>
2022-09-03 21:45:41 +0530

    Modification

---
+++ dataTypes.java
@@ -0,0 +1,12 @@
+public class dataTypes {
+
+    public static void main(String[] args) {
+        
+        int x = 123; //initialization
+        char sym = '@';
+        String msg = "My password is : ";
+        String name = "Anjan";
+        System.out.println(msg+name+sym+x); //string concatenation
+        
+    }
+}
